480e77e8c2694ed1ebd140507c912b0baad5d4c8
chaeyeon kim <kcy85078507@gmail.com>
2021-10-08 14:39:36 +0900

    Img change

--- js/main.js
+++ js/main.js
@@ -29,6 +29,11 @@ const blackBg = document.querySelector(".black_bg");
 const hiddenNav = document.querySelector(".navbar__lists");
 const leftLogo = document.querySelector(".navbar__logo");
 
+const bagsCateg = document.querySelector(".store__categ--bags");
+const hairsCateg = document.querySelector(".store__categ--hairs");
+const bagsItems = document.querySelector(".store__bagContainer");
+const hairsItems = document.querySelector(".store__hairContainer");
+
 toggleBtn.addEventListener("click", function () {
   nav.classList.toggle("hidden");
   blackBg.classList.toggle("hidden");
@@ -44,4 +49,20 @@ cancelBtn.addEventListener("click", function () {
   leftLogo.classList.toggle("left-logo");
   document.querySelector("body").style.removeProperty("overflow");
   document.querySelector(".store__categ").style.removeProperty("visibility");
+});
+
+bagsCateg.addEventListener("click", function () {
+  bagsItems.classList.remove("hidden");
+  hairsItems.classList.add("hidden");
+  bagsCateg.style.textDecorationLine = "underline";
+  bagsCateg.style.textUnderlineOffset = "5px";
+  hairsCateg.style.textDecorationLine = "none";
+});
+
+hairsCateg.addEventListener("click", function () {
+  bagsItems.classList.add("hidden");
+  hairsItems.classList.remove("hidden");
+  hairsCateg.style.textDecorationLine = "underline";
+  hairsCateg.style.textUnderlineOffset = "5px";
+  bagsCateg.style.textDecorationLine = "none";
 });

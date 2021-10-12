77a09a70c73b1644e739d0cf706b94f7c77af50e
chaeyeon kim <kcy85078507@gmail.com>
2021-10-12 14:50:44 +0900

    css, html change

--- js/main.js
+++ js/main.js
@@ -1,27 +1,3 @@
-// window.onload = function () {
-//   function onClick() {
-//     document.querySelector(".sidenav").style.display = "block";
-//     document.querySelector(".black_bg").style.display = "block";
-//     document.querySelector(".navbar__lists").style.visibility = "hidden";
-//     document.querySelector(".navbar__logo").classList.add("left-logo");
-//     document.querySelector("body").style.overflow = "hidden";
-//     document.querySelector(".store__categ").style.visibility = "hidden";
-//   }
-//   function offClick() {
-//     document.querySelector(".sidenav").style.display = "none";
-//     document.querySelector(".black_bg").style.display = "none";
-//     document.querySelector(".navbar__lists").style.visibility = "visible";
-//     document.querySelector(".navbar__logo").classList.remove("left-logo");
-//     document.querySelector("body").style.removeProperty("overflow");
-//     document.querySelector(".store__categ").style.removeProperty("visibility");
-//   }
-
-//   document
-//     .querySelector(".navbar__toggleBtn")
-//     .addEventListener("click", onClick);
-//   document.querySelector(".cancel").addEventListener("click", offClick);
-// };
-
 const toggleBtn = document.querySelector(".navbar__toggleBtn");
 const nav = document.querySelector(".sidenav");
 const cancelBtn = document.querySelector(".cancel");
